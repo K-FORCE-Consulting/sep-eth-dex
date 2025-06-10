ddd14ec1f76ec6d35c29fc0f12d60f4649ec1ce4
CyberFM <doyoumftu@gmail.com>
2025-06-10 00:49:31 -0400

    Update index.tsx

--- src/components/PhishingWarningBanner/index.tsx
+++ src/components/PhishingWarningBanner/index.tsx
@@ -148,7 +148,7 @@ const SpeechBubble = styled.div`
   }
 `
 
-const domain = 'https://sepoliadex.eth.link'
+const domain = 'https://dex.k-forceglobal.com'
 
 const PhishingWarningBanner: React.FC<React.PropsWithChildren> = () => {
   const { t } = useTranslation()
@@ -156,7 +156,7 @@ const PhishingWarningBanner: React.FC<React.PropsWithChildren> = () => {
   const { isMobile, isMd } = useMatchBreakpoints()
   const warningTextAsParts = useMemo(() => {
     const warningText = t("please make sure you're visiting %domain% - check the URL carefully.", { domain })
-    return warningText.split(/(https:\/\/pepepal.com)/g)
+    return warningText.split(/(https:\/\/dex.k-forceglobal.com)/g)
   }, [t])
   const warningTextComponent = (
     <>
